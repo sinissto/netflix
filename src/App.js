4806d2e478949e1d46853fd78110407500512ef0
Sinisa <sinisha.stojanovic@gmail.com>
2022-12-24 22:17:43 +0100

    Register page finished

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.scss";
 import Home from "./pages/home/Home";
 import Watch from "./pages/watch/Watch";
+import Register from "./pages/register/Register";
 
 function App() {
-  return <Watch />;
+  return <Register />;
 }
 
 export default App;
---
+++ src/pages/register/Register.js
@@ -0,0 +1,60 @@
+import "./Register.scss";
+import { useRef, useState } from "react";
+
+const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  const handleStart = () => {
+    // console.log(emailRef.current.value);
+    setEmail(emailRef.current.value);
+    emailRef.current.value = "";
+  };
+
+  const handleFinish = () => {
+    setPassword(passwordRef.current.value);
+  };
+
+  return (
+    <div className="register">
+      <div className="top">
+        <div className="wrapper">
+          <img
+            className="logo"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
+            alt=""
+          />
+          <button className="loginButton">Sign In</button>
+        </div>
+      </div>
+
+      <div className="container">
+        <h1>Unlimited movies, TV shows, and more.</h1>
+        <h2>Watch anywhere. Cancel anytime.</h2>
+        <p>
+          Ready to watch? Enter your email to create or restart your membership.
+        </p>
+        {!email && (
+          <div className="input">
+            <input type="email" placeholder="Mail address" ref={emailRef} />
+            <button className="registerButton" onClick={handleStart}>
+              Get started
+            </button>
+          </div>
+        )}
+        {email && (
+          <form className="input">
+            <input type="password" placeholder="Password" ref={passwordRef} />
+            <button className="registerButton" onClick={handleFinish}>
+              Finish
+            </button>
+          </form>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Register;
