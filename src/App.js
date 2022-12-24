5cfbc397368563dfecf0b9c517e793c0d3682aa5
Sinisa <sinisha.stojanovic@gmail.com>
2022-12-24 22:59:57 +0100

    Finished project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.scss";
 import Home from "./pages/home/Home";
 import Watch from "./pages/watch/Watch";
-import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
 
 function App() {
-  return <Register />;
+  return <Login />;
 }
 
 export default App;
