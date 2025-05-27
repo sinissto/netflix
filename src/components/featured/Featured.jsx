6b0ad09672e48bdec3e7af90b657081fe29330ab
Sinisa <sinisha.stojanovic@gmail.com>
2025-05-27 23:08:30 +0200

    Feature component markdown finished

---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,40 @@
+import "./featured.scss";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import InfoOutlineIcon from "@mui/icons-material/InfoOutline";
+
+const Featured = () => {
+  return (
+    <div className={"featured"}>
+      <img
+        className={"test-slika"}
+        src="https://images.pexels.com/photos/32145474/pexels-photo-32145474/free-photo-of-elegant-portrait-of-woman-with-headscarf.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        alt="User Image"
+      />
+
+      <div className="info">
+        <img
+          src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/10d7e8b1-9fe1-4c41-aeb7-331f4fb188aa/desxzmc-7d5eaf94-1c8f-4d62-b30d-9b1e11b85730.png/v1/fill/w_1280,h_544/the_matrix_logo_by_huyvo2001_desxzmc-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NTQ0IiwicGF0aCI6IlwvZlwvMTBkN2U4YjEtOWZlMS00YzQxLWFlYjctMzMxZjRmYjE4OGFhXC9kZXN4em1jLTdkNWVhZjk0LTFjOGYtNGQ2Mi1iMzBkLTliMWUxMWI4NTczMC5wbmciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.uChjzAOgKvW4-rcC1bTZ9GNi4Q8M6OPTjsILQjQQt40"
+          alt=""
+        />
+        <span className="desc">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. A at beatae
+          blanditiis cumque deserunt dolorem ea harum magni nisi nulla
+          praesentium quae quam qui quidem quis reprehenderit, sint, sit
+          voluptate.
+        </span>
+        <div className="buttons">
+          <button className={"play"}>
+            <PlayArrowIcon />
+            <span>Play</span>
+          </button>
+          <button className={"more-info"}>
+            <InfoOutlineIcon />
+            <span>More Info</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Featured;
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,15 +1,12 @@
 import "./home.scss";
 import Navbar from "../components/navbar/Navbar.jsx";
+import Featured from "../components/featured/Featured.jsx";
 
 const Home = () => {
   return (
     <div className={"home"}>
       <Navbar />
-      <img
-        className={"test-slika"}
-        src="https://images.pexels.com/photos/32220056/pexels-photo-32220056/free-photo-of-elegant-portrait-of-a-woman-with-gold-necklace.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-        alt="User Image"
-      />
+      <Featured />
     </div>
   );
 };
